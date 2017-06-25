a568b671c4531a8c7bf90d8044220a829f2f44a8
Arik Liber <leonliber@gmail.com>
2017-06-25 19:50:59 +0300

    Add support for same-element paragraphs

--- simoti.js
+++ simoti.js
@@ -12,7 +12,7 @@
   }
 
   function findContainerElements(beforeText, afterText) {
-    var elements = document.getElementsByTagName("*");
+    var elements = document.getElementsByTagName('*');
     var beforeContainers = [];
     var afterContainers = [];
 
@@ -20,7 +20,8 @@
     for (var i = 0; i < elements.length; i++) {
       if (elements[i].textContent.indexOf(beforeText) !== -1) {
         beforeContainers.push(elements[i]);
-      } else if(elements[i].textContent.indexOf(afterText) !== -1) {
+      }
+      if(elements[i].textContent.indexOf(afterText) !== -1) {
         afterContainers.push(elements[i]);
       }
     }
@@ -28,10 +29,14 @@
     //Find container elements
     var beforeElement = beforeContainers[beforeContainers.length - 1];
     var afterElement = afterContainers[afterContainers.length - 1];
-    for(i = beforeContainers.length - 1, j = afterContainers.length - 1; i >= 0 && j >= 0; i--, j--) {
-      if(beforeContainers[i] === afterContainers[j]) {
-        beforeElement = beforeContainers[i+1];
-        afterElement = afterContainers[i+1];
+
+    if(beforeElement !== afterElement) { // Elements are in different containers
+      for(i = beforeContainers.length - 1, j = afterContainers.length - 1; i >= 0 && j >= 0; i--, j--) {
+        if(beforeContainers[i] === afterContainers[j]) {
+          beforeElement = beforeContainers[i+1];
+          afterElement = afterContainers[i+1];
+          break;
+        }
       }
     }
 
@@ -42,16 +47,35 @@
 
   }
 
+  function placeSnippetHTML(containerElement, contentAfter, snippetHTML) {
+    var contentAfterIndex = containerElement.innerHTML.indexOf(contentAfter);
+    if(contentAfterIndex > -1) {
+      var articleBefore = containerElement.innerHTML.substring(0, contentAfterIndex);
+      var articleAfter = containerElement.innerHTML.substring(contentAfterIndex);
+      containerElement.innerHTML = articleBefore + snippetHTML + articleAfter;
+    }
+  }
+
+  function placeSnippetElement(beforeElement, afterElement, position, snippetHTML) {
+    var parser = new DOMParser()
+    var snippetElement = parser.parseFromString(snippetHTML, 'text/xml');
+
+    if(position === 'afterBeforeContent') {
+      beforeElement.parentNode.insertBefore(snippetElement.documentElement, beforeElement.nextSibling);
+    } else if(position === 'beforeAfterContent') {
+      afterElement.parentNode.insertBefore(snippetElement.documentElement, afterElement);
+    }
+  }
+
   function placeSnippet(snippet) {
     var containerElements = findContainerElements(snippet.contentBefore, snippet.contentAfter);
-    var parser = new DOMParser()
-    var snippetElement = parser.parseFromString(snippet.html, 'text/xml');
 
-    if(snippet.position === 'afterBeforeContent') {
-      containerElements.beforeElement.parentNode.insertBefore(snippetElement.documentElement, containerElements.beforeElement.nextSibling);
-    } else if(snippet.position === 'beforeAfterContent') {
-      containerElements.afterElement.parentNode.insertBefore(snippetElement.documentElement, containerElements.afterElement);
+    if(containerElements.beforeElement === containerElements.afterElement) { // Same parent - insert with between text
+      placeSnippetHTML(containerElements.afterElement, snippet.contentAfter, snippet.html)
+    } else { // Sperate elements (as should be) - add element between
+      placeSnippetElement(containerElements.beforeElement, containerElements.afterElement, snippet.position, snippet.html)
     }
+
   }
 
   getRequest('https://us-central1-simoti-171512.cloudfunctions.net/getSnippet', function(request){
