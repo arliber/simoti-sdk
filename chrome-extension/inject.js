dcee5c5b8aac3afa0c9d6103ef9ea49e67b5bb98
Arik Liber <leonliber@gmail.com>
2017-07-02 15:18:58 +0300

    add chrome extension to inject snippet

---
+++ chrome-extension/inject.js
@@ -0,0 +1,7 @@
+(function() {
+
+	var simoti = document.createElement('script');
+	simoti.setAttribute('src','https://snips.simoti.co/simoti.js');
+	document.head.appendChild(simoti);
+
+})();
---
+++ inject-code.js
@@ -0,0 +1,3 @@
+var simoti = document.createElement('script');
+simoti.setAttribute('src','https://snips.simoti.co/simoti.js');
+document.head.appendChild(simoti);
