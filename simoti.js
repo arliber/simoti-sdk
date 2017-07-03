6e1247eb16ec8367c3dce53da15c593bb0f2ffe4
Arik Liber <leonliber@gmail.com>
2017-07-03 17:11:37 +0300

    Add tracking support

--- simoti.js
+++ simoti.js
@@ -1,186 +1,229 @@
-  (function(){
+(function(){
 
-    report('info', 'root', 'loaded, waiting for ready state');
+  report('info', 'root', 'loaded, waiting for ready state');
 
-    // Vars
-    var waitTime = 0;
-    var waitInterval = 10;
-    var maxWaitTime = 5000;
-    var snippetUrl = 'https://snips.simoti.co/getSnippet';
-    var logUrl = 'https://snips.simoti.co/tagLogger';
+  // Vars
+  var waitTime = 0;
+  var waitInterval = 10;
+  var maxWaitTime = 5000;
+  var snippetUrl = 'https://snips.simoti.co/getSnippet';
+  var logUrl = 'https://snips.simoti.co/tagLogger';
 
-    // Init wait loop
-    var canStartInterval = setInterval(function() {
-      waitTime += waitInterval;
-      if (document.readyState === 'complete' || document.readyState === 'interactive') {
-          clearInterval(canStartInterval);
-          report('info', 'canStartInterval', 'starting after ['+waitTime+'ms] wait');
-          init();
-      } else if(waitTime >= maxWaitTime) {
+  // Init wait loop
+  var canStartInterval = setInterval(function() {
+    waitTime += waitInterval;
+    if (document.readyState === 'complete' || document.readyState === 'interactive') {
         clearInterval(canStartInterval);
-        report('warning', 'canStartInterval', 'max wait time reached, starting execution with state ['+document.readyState+']');
+        report('info', 'canStartInterval', 'starting after ['+waitTime+'ms] wait');
         init();
-      }
-    }, waitInterval);
-
-    // Functions
-    function report() {
-      console.log('Simoti - report: ', arguments);
-      arguments.ts = new Date().valueOf();
-      postRequest('https://us-central1-simoti-171512.cloudfunctions.net/tagLogger', arguments, function(err, result) {
-        if(err) {
-          console.log('Simoti: Unable to send log', err);
-        }
-      });
+    } else if(waitTime >= maxWaitTime) {
+      clearInterval(canStartInterval);
+      report('warning', 'canStartInterval', 'max wait time reached, starting execution with state ['+document.readyState+']');
+      init();
     }
+  }, waitInterval);
 
-    function init() {
-      getRequest(snippetUrl, function(err, response){
-        if(err) {
-          report('error', 'init', 'error in getRequest', err);
+  // Functions
+  function report() {
+    console.log('Simoti - report: ', arguments);
+    arguments.ts = new Date().valueOf();
+    postRequest('https://us-central1-simoti-171512.cloudfunctions.net/tagLogger', arguments, function(err, result) {
+      if(err) {
+        console.log('Simoti: Unable to send log', err);
+      }
+    });
+  }
+
+  function init() {
+    registerEvents();
+    getRequest(snippetUrl, function(err, response){
+      if(err) {
+        report('error', 'init', 'error in getRequest', err);
+      } else {
+        report('info', 'init', 'got response from server', response);
+        if(isResponseValid(response)) {
+          placeSnippet(response);
         } else {
-          report('info', 'init', 'got response from server', response);
-          if(isResponseValid(response)) {
-            placeSnippet(response);
-          } else {
-            report('info', 'init', 'response from server is not a valid snippet');
-          }
+          report('info', 'init', 'response from server is not a valid snippet');
         }
-      });
-    }
+      }
+    });
+  }
 
-    function isResponseValid(response) {
-      return response && response.contentBefore && response.html && response.status && response.status === 'active' && response.position && response.contentAfter;
-    }
+  function registerEvents() {
+    window.simotiClick = function(trackingUrl) {
+      report('info', 'simotiClick', 'sending click event', trackingUrl);
+      getRequest(trackingUrl, function(err, response) {
+        if(err) {
+          report('error', 'simotiClick', 'error sending click event', trackingUrl, err);
+        } else {
+          report('info', 'simotiClick', 'click event sent', trackingUrl, response);
+        }
+      }, true);
+    };
+  }
 
-    function getRequest(url, callback) {
-      var xhr = new XMLHttpRequest();
-      if (!('withCredentials' in xhr)) xhr = new XDomainRequest(); // fix IE8/9
-      xhr.open('GET', url);
-      xhr.onload = function(request) {
+  function isResponseValid(response) {
+    return response && response.contentBefore && response.html && response.status && response.status === 'active' && response.position && response.contentAfter;
+  }
+
+  function getRequest(url, callback, rawResponse) {
+    var xhr = new XMLHttpRequest();
+    if (!('withCredentials' in xhr)) xhr = new XDomainRequest(); // fix IE8/9
+    xhr.open('GET', url);
+    xhr.onload = function(request) {
+      var response = request.currentTarget.response || request.target.responseText;
+      if(rawResponse) {
+        callback(null, response);
+      } else {
         try  {
-          var response = JSON.parse(request.currentTarget.response || request.target.responseText);
-          callback(null, response);
+          var JSONResponse = JSON.parse(response);
+          callback(null, JSONResponse);
         } catch (e) {
           callback(e);
         }
-      };
-      xhr.onerror = function() {
-        callback({error: 'unable to send get request'})
-      };
-      xhr.send();
-      return xhr;
-    }
+      }
+    };
+    xhr.onerror = function() {
+      callback({error: 'unable to send get request'})
+    };
+    xhr.send();
+    return xhr;
+  }
 
-    function postRequest(url, data, callback) {    
-      var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-      xhr.open('POST', url, true);
-      xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-      xhr.setRequestHeader('Content-type', 'application/json');
-      xhr.onload = function(request) {
+  function getRequest(url, callback, rawResponse) {
+    var xhr = new XMLHttpRequest();
+    if (!('withCredentials' in xhr)) xhr = new XDomainRequest(); // fix IE8/9
+    xhr.open('GET', url);
+    xhr.onload = function(request) {
+      var response = request.currentTarget.response || request.target.responseText;
+      if(rawResponse) {
+        callback(null, response);
+      } else {
         try  {
-          var response = JSON.parse(request.currentTarget.response || request.target.responseText);
-          callback(null, response);
+          var JSONResponse = JSON.parse(response);
+          callback(null, JSONResponse);
         } catch (e) {
           callback(e);
         }
-      };
-      xhr.onerror = function() {
-        callback({error: 'unable to send post request'})
-      };
-      xhr.send(JSON.stringify(data));
-      return xhr;
+      }
+    };
+    xhr.onerror = function() {
+      callback({error: 'unable to send get request'})
+    };
+    xhr.send();
+    return xhr;
+  }
+
+  function postRequest(url, data, callback) {    
+    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.onload = function(request) {
+      try  {
+        var response = JSON.parse(request.currentTarget.response || request.target.responseText);
+        callback(null, response);
+      } catch (e) {
+        callback(e);
+      }
+    };
+    xhr.onerror = function() {
+      callback({error: 'unable to send post request'})
+    };
+    xhr.send(JSON.stringify(data));
+    return xhr;
+  }
+
+  function findContainerElements(beforeText, afterText) {
+    var elements = document.getElementsByTagName('*');
+    var beforeContainers = [];
+    var afterContainers = [];
+
+    //Build container trees
+    for (var i = 0; i < elements.length; i++) {
+      if (elements[i].textContent.indexOf(beforeText) !== -1) {
+        beforeContainers.push(elements[i]);
+      }
+      if(elements[i].textContent.indexOf(afterText) !== -1) {
+        afterContainers.push(elements[i]);
+      }
     }
 
-    function findContainerElements(beforeText, afterText) {
-      var elements = document.getElementsByTagName('*');
-      var beforeContainers = [];
-      var afterContainers = [];
+    //Find container elements
+    var beforeElement = beforeContainers[beforeContainers.length - 1];
+    var afterElement = afterContainers[afterContainers.length - 1];
 
-      //Build container trees
-      for (var i = 0; i < elements.length; i++) {
-        if (elements[i].textContent.indexOf(beforeText) !== -1) {
-          beforeContainers.push(elements[i]);
-        }
-        if(elements[i].textContent.indexOf(afterText) !== -1) {
-          afterContainers.push(elements[i]);
+    if(beforeElement !== afterElement) { // Elements are in different containers
+      for(i = beforeContainers.length - 1, j = afterContainers.length - 1; i >= 0 && j >= 0; i--, j--) {
+        if(beforeContainers[i] === afterContainers[j]) {
+          beforeElement = beforeContainers[i+1];
+          afterElement = afterContainers[i+1];
+          break;
         }
       }
-
-      //Find container elements
-      var beforeElement = beforeContainers[beforeContainers.length - 1];
-      var afterElement = afterContainers[afterContainers.length - 1];
-
-      if(beforeElement !== afterElement) { // Elements are in different containers
-        for(i = beforeContainers.length - 1, j = afterContainers.length - 1; i >= 0 && j >= 0; i--, j--) {
-          if(beforeContainers[i] === afterContainers[j]) {
-            beforeElement = beforeContainers[i+1];
-            afterElement = afterContainers[i+1];
-            break;
-          }
-        }
-      }
-
-      return {
-        beforeElement: beforeElement,
-        afterElement: afterElement
-      };
-
     }
 
-    function placeSnippetHTML(containerElement, contentBefore, contentAfter, position, snippetHTML) {
-      var elementHTML = containerElement.innerHTML;
-      var contentBeforeIndex = containerElement.innerHTML.indexOf(contentBefore);
-      var contentAfterIndex = containerElement.innerHTML.indexOf(contentAfter);
-      
-      var regex = /<\s*br\s*\/?>/gi;
-      var insertPosition = -1;
-      while ((match = regex.exec(elementHTML)) != null && insertPosition === -1) {
-        var brIndex = match.index;
-        if(brIndex > contentBeforeIndex && brIndex < contentAfterIndex) { // found a <br> between the elements
-          if(position === 'afterBeforeContent') {
-            insertPosition  = brIndex;
-          } else if(position === 'beforeAfterContent') {
-            insertPosition = brIndex + match[0].length;
-          }
-        } else if(brIndex > contentAfterIndex) { // no <br> between paragraphs - insert AFTER next <br>
+    return {
+      beforeElement: beforeElement,
+      afterElement: afterElement
+    };
+
+  }
+
+  function placeSnippetHTML(containerElement, contentBefore, contentAfter, position, snippetHTML) {
+    var elementHTML = containerElement.innerHTML;
+    var contentBeforeIndex = containerElement.innerHTML.indexOf(contentBefore);
+    var contentAfterIndex = containerElement.innerHTML.indexOf(contentAfter);
+    
+    var regex = /<\s*br\s*\/?>/gi;
+    var insertPosition = -1;
+    while ((match = regex.exec(elementHTML)) != null && insertPosition === -1) {
+      var brIndex = match.index;
+      if(brIndex > contentBeforeIndex && brIndex < contentAfterIndex) { // found a <br> between the elements
+        if(position === 'afterBeforeContent') {
+          insertPosition  = brIndex;
+        } else if(position === 'beforeAfterContent') {
           insertPosition = brIndex + match[0].length;
         }
-      }
-
-      // Insert into calculated position
-      if(insertPosition !== -1) {
-        var articleBefore = containerElement.innerHTML.substring(0, insertPosition);
-        var articleAfter = containerElement.innerHTML.substring(insertPosition);
-        containerElement.innerHTML = articleBefore + snippetHTML + articleAfter;
-      } else {
-        report('error', 'placeSnippet', 'unable to find mathing location for snippet', arguments);
-      }
-      
-    }
-
-    function placeSnippetElement(beforeElement, afterElement, position, snippetHTML) {
-      snippetElement = tempdiv = document.createElement('div');
-      snippetElement.innerHTML = snippetHTML;
-
-      if(position === 'afterBeforeContent') {
-        beforeElement.parentNode.insertBefore(snippetElement.firstChild, beforeElement.nextSibling);
-      } else if(position === 'beforeAfterContent') {
-        afterElement.parentNode.insertBefore(snippetElement.firstChild, afterElement);
+      } else if(brIndex > contentAfterIndex) { // no <br> between paragraphs - insert AFTER next <br>
+        insertPosition = brIndex + match[0].length;
       }
     }
 
-    function placeSnippet(snippet) {
-      var containerElements = findContainerElements(snippet.contentBefore, snippet.contentAfter);
+    // Insert into calculated position
+    if(insertPosition !== -1) {
+      var articleBefore = containerElement.innerHTML.substring(0, insertPosition);
+      var articleAfter = containerElement.innerHTML.substring(insertPosition);
+      containerElement.innerHTML = articleBefore + snippetHTML + articleAfter;
+    } else {
+      report('error', 'placeSnippet', 'unable to find mathing location for snippet', arguments);
+    }
+    
+  }
 
-      if(containerElements.beforeElement === containerElements.afterElement) { // Same parent - insert with between text
-        console.log('Same parent - insert with between text')
-        placeSnippetHTML(containerElements.afterElement, snippet.contentBefore, snippet.contentAfter, snippet.position, snippet.html);
-      } else { // Sperate elements (as should be) - add element between
-        console.log('Sperate elements (as should be) - add element between')
-        placeSnippetElement(containerElements.beforeElement, containerElements.afterElement, snippet.position, snippet.html);
-      }
+  function placeSnippetElement(beforeElement, afterElement, position, snippetHTML) {
+    snippetElement = tempdiv = document.createElement('div');
+    snippetElement.innerHTML = snippetHTML;
 
+    if(position === 'afterBeforeContent') {
+      beforeElement.parentNode.insertBefore(snippetElement.firstChild, beforeElement.nextSibling);
+    } else if(position === 'beforeAfterContent') {
+      afterElement.parentNode.insertBefore(snippetElement.firstChild, afterElement);
+    }
+  }
+
+  function placeSnippet(snippet) {
+    var containerElements = findContainerElements(snippet.contentBefore, snippet.contentAfter);
+
+    if(containerElements.beforeElement === containerElements.afterElement) { // Same parent - insert with between text
+      console.log('Same parent - insert with between text')
+      placeSnippetHTML(containerElements.afterElement, snippet.contentBefore, snippet.contentAfter, snippet.position, snippet.html);
+    } else { // Sperate elements (as should be) - add element between
+      console.log('Sperate elements (as should be) - add element between')
+      placeSnippetElement(containerElements.beforeElement, containerElements.afterElement, snippet.position, snippet.html);
     }
 
-  })();
+  }
+
+})();
